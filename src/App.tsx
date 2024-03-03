4f6bae74945fc79fcad7d7d5318cf0076695a65b
Marine <marine.guyodo@gmail.com>
2024-03-03 11:20:00 +0100

    Add prettier plugin for tailwindcss to sort classes + test it on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Teetsh</h1>
+    <div className="flex h-screen flex-col">
+      <header className="flex items-center justify-center gap-8 bg-muted p-4">
+        <h1 className="font-extrabold">Teetsh</h1>
+      </header>
     </div>
   );
 }
