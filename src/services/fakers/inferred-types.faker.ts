62a92a093f32ba6594bcf9c0d747c58777d4942e
Marine <marine.guyodo@gmail.com>
2024-03-03 15:59:17 +0100

    Create faker and inferred types from response

---
+++ src/services/fakers/inferred-types.faker.ts
@@ -0,0 +1,11 @@
+import faker from "@/services/fakers/response.faker.json";
+
+export type Programmation = typeof faker.data;
+
+export type Periode = (typeof faker.data.attributes.periodes)[number];
+
+export type Matiere = (typeof faker.data.attributes.matieres)[number];
+
+export type Domaine = Matiere["domaines"][number];
+
+export type Item = Matiere["domaines"][number]["items"][number];
--- src/services/programmations.services.ts
+++ src/services/programmations.services.ts
@@ -1,18 +1,21 @@
-async function getProgrammation(id: number): Promise<unknown> {
-  // throw new Error("Programmation introuvable...");
+import faker from "@/services/fakers/response.faker.json";
+import { Programmation } from "@/services/fakers/inferred-types.faker";
 
-  const response = await fetch(
-    "https://strapi.teetsh.com/api/programmations/" + id,
-    {
-      headers: {
-        Authorization: "Bearer " + import.meta.env.VITE_TOKEN
-      }
-    }
-  );
-  const { data, meta } = await response.json();
+async function getProgrammation(): Promise<Programmation> {
+  return faker.data;
 
-  console.log("META:", meta, "ID:", id);
-  return data;
+  // const response = await fetch(
+  //   "https://strapi.teetsh.com/api/programmations/" + id,
+  //   {
+  //     headers: {
+  //       Authorization: "Bearer " + import.meta.env.VITE_TOKEN
+  //     }
+  //   }
+  // );
+  // const { data, meta } = await response.json();
+
+  // console.log("META:", meta, "ID:", id);
+  // return data;
 }
 
 export { getProgrammation };
