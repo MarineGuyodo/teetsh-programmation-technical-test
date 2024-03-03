ff8381636217986a654f9e83c7cdf1ec1be5cb95
Marine <marine.guyodo@gmail.com>
2024-03-03 15:24:46 +0100

    Install and configure react-query + corresponding eslint plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
+// PROVIDERS
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./components/providers/theme-provider.tsx";
 
+// APP MAIN COMPONENT
+import App from "./App.tsx";
+
+// STYLES
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
