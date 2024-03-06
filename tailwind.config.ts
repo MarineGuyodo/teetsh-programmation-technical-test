520b568de458071df76cebfe0386c8219859a6ef
Marine <marine.guyodo@gmail.com>
2024-03-06 09:29:01 +0100

    Ensure project can be built + fixes some minor issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,9 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      listStyleImage: {
+        check: "url('@/assets/check.svg')"
       }
     }
   },
